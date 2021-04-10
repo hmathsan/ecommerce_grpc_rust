1706351fa6cd7727bc86de52a4643a521986126b
Matheus Santos <matheus.san99@gmail.com>
2021-04-09 21:40:52 -0300

    basic hello world implementation

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_client(false)
+        .build_server(true)
+        .compile(&["proto\\helloworld.proto"], &["proto"])?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloRequest, HelloResponse};
+
+pub mod hello_world {
+    include!(concat!(env!("OUT_DIR"), "\\helloworld.rs"));
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloResponse{
+            message: format!("Hello {}!", request.into_inner().name).into()
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    run_server().await?;
+
+    Ok(())
+}
+
+async fn run_server() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    println!("Listening to address: {}", addr);
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
